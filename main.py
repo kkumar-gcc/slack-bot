c4144a666735c98ae26e6f31723c6f31bf26e3c7
kkumar-gcc <krishkumar9352@gmail.com>
2023-07-15 10:54:48 +0530

    make app port dynamic

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
-# main.py
-
+import os
 import uvicorn
+
 from fastapi import FastAPI
 from app.api.routes import router
-from fastapi import Request
-from app.bots.slack_bot import app_handler
-from app.bots.slack_bot import app as slack_app
 
 api = FastAPI()
 
 # Mount the API router
 api.include_router(router)
 
-
-@router.post("/slack/events")
-async def endpoint(req: Request):
-    return await app_handler.handle(req)
-
-
 # Run the FastAPI server
 if __name__ == "__main__":
+    # Get the port from the environment variable or use the default value
+    port = int(os.getenv("APP_PORT", 8000))
+
     # Start the FastAPI server
-    uvicorn.run("main:api", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:api", host="0.0.0.0", port=port, reload=True)
